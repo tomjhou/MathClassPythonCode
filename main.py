180206fe1273ad246b9d9995f7d1c1c69cca7738
Thomas Jhou <tomjhou@gmail.com>
2021-09-22 12:31:21 -0400

    Simplified code (calculates only one mean instead of two)

--- main.py
+++ main.py
@@ -14,23 +14,17 @@ time_values = photo_array[:,0]
 # Calcium measurements in columns from 2 until the last
 calcium = photo_array[:,1:]
 
-print(f"Array shape is {np.shape(photo_array)}")
-
-# Average all calcium values in each column
-m1 = np.mean(calcium, axis = 0)
-num_trials = np.size(m1)
-print(f"Calculated average for {num_trials} trials, one in each column")
-
-# Average all values in each row
-m2 = np.mean(calcium, axis = 1)
-print(f"Calculated average for {np.size(m2)} time points, one in each row")
-
+# Plot each trial
 plt.plot(time_values, calcium, linewidth=0.5)
 plt.xlabel("Time after cue onset (s)")
 plt.title("Calcium measurements for each trial")
 
 # Create new figure, otherwise plots will overlap on same one
 plt.figure()
-plt.plot(time_values, m2)
+
+# Plot average across trials
+# Average all values in each row
+row_means = np.mean(calcium, axis = 1)
+plt.plot(time_values, row_means)
 plt.xlabel("Time after cue onset (s)")
-plt.title(f"Calcium measurements averaged across all {num_trials} trials")
+plt.title(f"Calcium measurements averaged across all {np.shape(calcium)[1]} trials")
