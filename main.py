a497f8de32d7390d15fa823dd5ed43021276304a
Thomas Jhou <tomjhou@gmail.com>
2021-09-26 21:33:03 -0400

    Updated comments

--- main.py
+++ main.py
@@ -9,25 +9,28 @@ photo_frame = pd.read_excel("photometry.xlsx")
 # Convert to numpy array for easier indexing
 photo_array = photo_frame.to_numpy()
 
-# Timestamps in first column
+# Extract timestamps from first column
 time_values = photo_array[:,0]
 
-# Calcium measurements in columns from 2 until the last
+# Extract calcium measurements from columns 2 until end
 calcium = photo_array[:,1:]
 
-# Plot each trial
+# Plot each trial as a separate line
 plt.plot(time_values, calcium, linewidth=0.5)
+# Add x-axis label
 plt.xlabel("Time after cue onset (s)")
+# Add title
 plt.title("Calcium measurements for each trial")
 
-# Create new figure, otherwise plots will overlap on same one
+# Now create new figure, otherwise subsequent plots will overlap previous
 plt.figure()
 
-# Plot average across trials
-# Average all values in each row
+# Average all values for each time point
 row_means = np.mean(calcium, axis = 1)
+# Plot average across trials
 plt.plot(time_values, row_means)
 plt.xlabel("Time after cue onset (s)")
 plt.title(f"Calcium measurements averaged across all {np.shape(calcium)[1]} trials")
 
+# If you haven't selected "run with Python console" option, you need this or else plot won't show.
 plt.show()
