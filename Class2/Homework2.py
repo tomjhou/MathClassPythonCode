ae1d35265064c7044f8f800501eba5c6e739456e
Thomas Jhou <tomjhou@gmail.com>
2021-09-28 10:43:37 -0400

    Added more class2 materials, added class 1 supplement.

--- Class2/Homework2.py
+++ Class2/Homework2.py
@@ -14,71 +14,48 @@ nyquistRate = sampleRate / 2
 # Get # of samples in waveform
 len_w = len(wave)
 
+print(f'Loaded waveform with {len_w} samples = {len_w / sampleRate} seconds.')
+print(f'Now playing through computer speaker. You should hear a piercing tone.')
+
+sd.play(wave)
+
+
 # Calculate FFT of raw waveform.
-f = abs(np.fft.fft(wave))  # Take absolute value to avoid worrying about imaginary components
+f = np.abs(np.fft.fft(wave))  # Absolute value allows us to ignore phase
 
 # plot FFT vs frequency
-# plt.figure()
 freq_list = np.arange(len_w) / len_w * sampleRate   # list of frequencies
 plt.plot(freq_list, f)
 plt.title('FFT of input waveform')
 
-# Find noise amplitude
-noiseIndex = round(2000 / sampleRate * len_w)
-print(f"Amplitude of 2kHz noise is {abs(f[noiseIndex])}\n")
-
-#  Second method of finding noise amplitude
-maxIndex = np.argmin(f)
-maxValue = f[maxIndex]
-print(f'Amplitude of peak is {maxValue}.\n')
-
-# Sample FFT plot as before, but with logarithmic x-axis, and with second
-# half truncated, as it is just a mirror image of first half.
+# Sample FFT plot as before, but with log axis, and with second
+# half deleted, as it is mirror image of first half.
 plt.figure()
-halfLen = int(np.floor(len_w / 2))  # Need floor function to make sure this is integer, or else next line will fail
-plt.plot(freq_list[0:halfLen], np.abs(f[:halfLen]))  # Show only first half
+halfLen = len_w // 2    # We use // instead of /, to force result to be integer
+plt.plot(freq_list[0:halfLen], f[0:halfLen])  # Show only first half
 plt.xscale('log')
 plt.title('FFT of input, log scale')
 
-# Solution: bandstop filter
-order = 50001
-margin = 400
-w = signal.firwin(order, np.array([2000 - margin, 2000 + margin])/nyquistRate)
-
+# Solution: crappy low-pass filter with cutoff at 1500 Hz
+order = 51  # Highly recommend using an odd number for filter order.
+print(f'Starting to calculate convolution...')
+w = signal.firwin(order, 1500/nyquistRate)
 wave_Filtered = np.convolve(wave, w)
+print(f'Done calculating convolution.')
 
-# plot impulse response. Truncate the endpoints
-plt.figure()
-range1 = round(order*.49)
-range2 = round(order*.51)
-plt.plot(np.arange(range1, range2), w[range1:range2])
-plt.title('Impulse response, bandstop filter')
 
-# plot frequency response
-plt.figure()
-fft_w = np.abs(np.fft.fft(w))
-len_w = len(fft_w)   # convolution increased length. Have to recalculate frequency list.
-freq_list = np.arange(len_w) / len_w * sampleRate   # list of frequencies
-halfLen = int(np.floor(len_w / 2))   # Need floor function to make sure this is integer
-plt.plot(freq_list[:halfLen], fft_w[:halfLen])  # Show only first half
-plt.xscale('log')
-plt.yscale('log')
-plt.title('Frequency response, bandstop filter')
-
-# plot FFT of final output
+# plot FFT of final output, log scales
 plt.figure()
 fft_filtered = abs(np.fft.fft(wave_Filtered))
-len_w = len(fft_filtered)   # convolution increased length. Have to recalculate frequency list.
-halfLen = round(len_w/2)
-freq_list = np.arange(len_w) / len_w * sampleRate    # ; % list of frequencies
-plt.plot(freq_list[:halfLen], fft_filtered[:halfLen])
+len_filtered = len(fft_filtered)   # convolution increased length. Have to recalculate frequency list.
+freq_list_filtered = np.arange(len_filtered) / len_filtered * sampleRate   # list of frequencies
+halfLen = len_filtered // 2
+plt.plot(freq_list_filtered[:halfLen], fft_filtered[:halfLen])
 plt.xscale('log')
 plt.yscale('log')
+plt.title('FFT after filter')
 
-noiseIndex = round(2000 / sampleRate * len_w)
-print(f'After filtering, amplitude of 2kHz noise is {fft_filtered[noiseIndex]}\n')
+print('Now playing filtered signal through computer speaker. It will still sound like a piercing tone.')
+print('Your homework is to improve this crappy filter to reveal the underlying musical piece.')
 
-plt.title('FFT after FIR filtering');
-
-sd.play(wave_Filtered, blocking=True)
-
+plt.show()
